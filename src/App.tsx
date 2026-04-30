454d18187fde76c593884a24f7d28622ebae61ea
Adit Dhanushkodi <adit.nd@gmail.com>
2026-04-30 15:29:00 -0700

    feat: add React Router setup and app entry point

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
-function App() {
-  return <div>App</div>
+import { Suspense } from 'react'
+import { BrowserRouter, MemoryRouter, Routes, Route } from 'react-router-dom'
+import { widgets } from './widgets'
+import Home from './pages/Home'
+
+interface Props {
+  initialPath?: string
 }
 
-export default App
+function AppRoutes() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {widgets.map((widget) => {
+          const Component = widget.component
+          return <Route key={widget.path} path={widget.path} element={<Component />} />
+        })}
+      </Routes>
+    </Suspense>
+  )
+}
+
+export default function App({ initialPath }: Props) {
+  if (initialPath !== undefined) {
+    return (
+      <MemoryRouter initialEntries={[initialPath]}>
+        <AppRoutes />
+      </MemoryRouter>
+    )
+  }
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  )
+}
