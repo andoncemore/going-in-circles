ac93d3195f1240dcc89a81ea364224951844d232
Adit Dhanushkodi <adit.nd@gmail.com>
2026-05-15 12:16:45 -0700

    feat: register logo widget at /logo as the first home gallery entry

--- src/widgets.ts
+++ src/widgets.ts
@@ -10,6 +10,12 @@ export interface Widget {
 
 export const widgets: Widget[] = [
   {
+    name: 'Roundabout Logo Generator',
+    description: 'Generate the Roundabout location logo as SVG or PNG',
+    path: '/logo',
+    component: lazy(() => import('./widgets/logo')),
+  },
+  {
     name: 'Newsletter Map',
     description: 'Generate a styled map graphic for newsletters',
     path: '/newsletter-map',
